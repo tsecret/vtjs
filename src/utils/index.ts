1c2bbb55b0e3a354214a95ade59e88c534cd77aa
Timur <36734601+tsecret@users.noreply.github.com>
2025-06-15 14:44:06 +0200

    working lockfile read

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,19 +1,25 @@
-import { BaseDirectory, exists, readFile } from '@tauri-apps/plugin-fs';
+import { BaseDirectory, exists } from '@tauri-apps/plugin-fs';
+import { localDataDir } from '@tauri-apps/api/path';
 import { hostname } from '@tauri-apps/plugin-os';
 export const isMac = () => navigator.platform.toUpperCase().indexOf('MAC') >= 0;
 
 export const readLockfile = async (): Promise<string> => {
   // C:\\Users\\${os.userInfo().username}\\AppData\\Local\\Riot Games\\Riot Client\\Config\\lockfile
-  const path = isMac() ? `./lockfile` : ``
+
 
   const currentPlatform = await hostname();
   console.log('hostname', currentPlatform);
 
-  const exist = await exists('\\Local\\Riot Games\\Riot Client\\Config\\lockfile', { baseDir: BaseDirectory.AppLocalData })
+  const path = await localDataDir()
+  console.log('path', path)
+
+  const exist = await exists(path + '\\Riot Games\\Riot Client\\Config\\lockfile')
   console.log('exists', exist)
 
-  const file = await readFile('\\Local\\Riot Games\\Riot Client\\Config\\lockfile', { baseDir: BaseDirectory.AppLocalData })
-  return file.toString()
+  // const file = await readFile('\\Local\\Riot Games\\Riot Client\\Config\\lockfile', { baseDir: BaseDirectory.AppData })
+  // return file.toString()
+
+  return ''
 }
 
 export const parseLockFile = (content: string): [string, string] => {
