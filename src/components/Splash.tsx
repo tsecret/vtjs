6d70d52877c188144b0cb16a44c5dffe20d73342
tsecret <timichfull@gmail.com>
2025-06-18 14:58:16 +0200

    update

--- src/components/Splash.tsx
+++ src/components/Splash.tsx
@@ -2,6 +2,5 @@
 export const Splash = () => {
   return <div className="h-screen w-screen flex flex-col items-center">
       <p className="m-auto text-6xl font-bold">Valorant Plus</p>
-      <p><span className="loading loading-spinner loading-xl m-auto"></span>Checking for updates</p>
   </div>
 }
